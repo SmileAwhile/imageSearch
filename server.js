04d6ea9eab3ef193326c11cab9c88daf55618d18
SmileAwhile <cjdunn11@gmail.com>
2016-07-11 19:49:00 -0700

    test mongodb

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express')
 var app = express();
+var url = process.env.MONGOLAB_URI;
+var mongo = require('mongodb').MongoClient;
 
 app.get('/', function (req, res) {
 
-    res.end("HELLO");
+    mongo.connect(url, function (err, db) {
+    if (err) {
+      res.end('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      res.end('Connection established to DB');
+
+      // do some work here with the database.
+
+      //Close connection
+      db.close();
+    }
+  });
 
 });
 
